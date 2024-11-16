38569baf50fee02aa0ca7eac1cb6a401940e86af
TheMas3212 <mas3212@gmail.com>
2024-11-16 19:31:58 +1100

    Improve connection state management

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,5 @@ if (process.env.TMUX === undefined) {
 
 const shellHost = process.argv[3] ?? "127.0.0.1";
 const shellPort = Number(process.argv[2] ?? "4242");
-const ipcPort = Number(process.argv[4] ?? "2424");
 
-catcher(shellHost, shellPort, ipcPort);
+catcher(shellHost, shellPort);
